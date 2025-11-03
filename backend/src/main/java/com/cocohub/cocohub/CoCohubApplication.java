523e9074029424f09f68020c9bf1e901a095f245
SakHack <sakshi2408jain@gmail.com>
2025-11-04 01:17:29 +0530

    added sample class

---
+++ backend/src/main/java/com/cocohub/cocohub/CoCohubApplication.java
@@ -0,0 +1,13 @@
+package com.cocohub.cocohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoCohubApplication.class, args);
+	}
+
+}
