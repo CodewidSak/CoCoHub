c21ba7b682a0e85fdf05e05dc2e79c5bf27f0319
SakHack <sakshi2408jain@gmail.com>
2025-11-04 01:17:53 +0530

    added demo class

---
+++ backend/src/test/java/com/cocohub/cocohub/CoCohubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocohub.cocohub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoCohubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
