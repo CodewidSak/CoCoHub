714bd1f247a89a347d8654daaa0e132df5a0ae8a
SakHack <sakshi2408jain@gmail.com>
2025-11-10 11:52:52 +0530

    created user controller

---
+++ backend/src/main/java/com/pmt/CoCoHub/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.pmt.CoCoHub.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pmt.CoCoHub.dto.UserSignInDTO;
+import com.pmt.CoCoHub.dto.UserSignUpDTO;
+import com.pmt.CoCoHub.model.User;
+import com.pmt.CoCoHub.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api")
+public class UserController {
+    
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity signUpUser(@RequestBody UserSignUpDTO userSingUpDTO) {
+        User user = userService.registerUser(userSingUpDTO);
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity signUpUser(@RequestBody UserSignInDTO userSingInDTO) {
+        User user = userService.signInUser(userSingInDTO);
+        return ResponseEntity.ok(user);
+    }
+    
+}
