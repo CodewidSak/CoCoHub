90a74d14cca6afbc2a16add0783b47932d8e0bc1
SakHack <sakshi2408jain@gmail.com>
2025-11-10 11:53:27 +0530

    created user entity

---
+++ backend/src/main/java/com/pmt/CoCoHub/model/User.java
@@ -0,0 +1,24 @@
+package com.pmt.CoCoHub.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)  // YE LINE HAI SABSE BADI
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
